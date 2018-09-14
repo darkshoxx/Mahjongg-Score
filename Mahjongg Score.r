2d5c571d79eb34ed24abc4077fe2a1c4db86650f
darkshoxx <43265727+darkshoxx@users.noreply.github.com>
2018-09-14 13:43:12 +0200

    Add files via upload

--- Mahjongg Score.r
+++ Mahjongg Score.r
@@ -3,16 +3,16 @@ load("C:\\Users\\Daniel1\\Desktop\\Mahjong\\zombie.rdata")
 
 #zombie=matrix(NA,0,9)
 #yonin=matrix(NA,0,9)
-#colnames(zombie)<-c("id","day","month","year","Daniel","Andreas","Theresa","NN7","Zombie")
-#colnames(yonin)<-c("id","day","month","year","Daniel","Andreas","Theresa","NN6","NN7")
+#colnames(zombie)<-c("id","day","month","year","Daniel","Andreas","Theresa","Tobi","Zombie","Kasumi")
+#colnames(yonin)<-c("id","day","month","year","Daniel","Andreas","Theresa","Tobi","NN7")
 
 
-add=function(typ,d,m,y,dan,and,the,nn,nz,test=1,Z=zombie,Y=yonin){
+add=function(typ,d,m,y,scores,test=1,Z=zombie,Y=yonin){
 if(typ==3){mat=Z}else{if(typ==4){mat=Y}else{return("ERROR");break()}}
 if(is.na(mat[1])){i=1}else{i=max(mat[,1]+1)}
-NEW=rbind(mat,c(i,d,m,y,dan,and,the,nn,nz))
+NEW=rbind(mat,c(i,d,m,y,scores))
 tsum=sum(NEW[i,5:ncol(NEW)])
-if(tsum>0.01){print("ERROR");break()}else{print(tsum)}
+if(tsum>0.01){print("ERRORSUM");break()}else{print(tsum)}
 if(test==1){print(NEW);print("THIS WAS A TEST")}else{if(typ==3){zombie<<-NEW}else{yonin<<-NEW};print(NEW)}
 }
 
@@ -25,8 +25,22 @@ return(M)
 }
 point(zombie)
 
- #add(nplay,day,mon,year,15.3,28.2,2.7,0,-46.2,0)
+addplayer=function(Mat,Name){
+MAT=cbind(Mat,0)
+colnames(MAT)=c(colnames(Mat),Name)
+return(MAT)
+}
 
+resortrelabel=function(M){
+M=M[order(M[,1]),]
+M[,1]<-1:nrow(M)
+
+return(M)
+}
+#zombie=resortrelabel(zombie)
+
+ #add(nplay,day,mon,year,c(15.3,28.2,2.7,0,-46.2),0)
+add(3,9,4,18,c(0,0,0,0,0,0),1)
 
 save(yonin,file="C:\\Users\\Daniel1\\Desktop\\Mahjong\\4nin.rdata")
 save(zombie,file="C:\\Users\\Daniel1\\Desktop\\Mahjong\\zombie.rdata")
