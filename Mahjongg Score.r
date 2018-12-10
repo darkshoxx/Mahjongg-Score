d89dfa7025e02cfa8c72cdf5e59215b20a770586
darkshoxx <43265727+darkshoxx@users.noreply.github.com>
2018-12-10 17:50:49 +0100

    Add files via upload

--- Mahjongg Score.r
+++ Mahjongg Score.r
@@ -3,7 +3,7 @@ load("C:\\Users\\Daniel1\\Desktop\\Mahjong\\zombie.rdata")
 
 #zombie=matrix(NA,0,9)
 #yonin=matrix(NA,0,9)
-#colnames(zombie)<-c("id","day","month","year","Daniel","Andreas","Theresa","Tobi","Zombie","Kasumi")
+#colnames(zombie)<-c("id","day","month","year","Daniel","Andreas","Theresa","Tobi","Zombie")
 #colnames(yonin)<-c("id","day","month","year","Daniel","Andreas","Theresa","Tobi","NN7")
 
 
@@ -12,7 +12,7 @@ if(typ==3){mat=Z}else{if(typ==4){mat=Y}else{return("ERROR");break()}}
 if(is.na(mat[1])){i=1}else{i=max(mat[,1]+1)}
 NEW=rbind(mat,c(i,d,m,y,scores))
 tsum=sum(NEW[i,5:ncol(NEW)])
-if(tsum>0.01){print("ERRORSUM");break()}else{print(tsum)}
+if(abs(tsum)>0.01){print("ERRORSUM");break()}else{print(tsum)}
 if(test==1){print(NEW);print("THIS WAS A TEST")}else{if(typ==3){zombie<<-NEW}else{yonin<<-NEW};print(NEW)}
 }
 
@@ -40,7 +40,7 @@ return(M)
 #zombie=resortrelabel(zombie)
 
  #add(nplay,day,mon,year,c(15.3,28.2,2.7,0,-46.2),0)
-add(3,9,4,18,c(0,0,0,0,0,0),1)
+add(3,8,10,18,c(0,0,0,0,0),1)
 
 save(yonin,file="C:\\Users\\Daniel1\\Desktop\\Mahjong\\4nin.rdata")
 save(zombie,file="C:\\Users\\Daniel1\\Desktop\\Mahjong\\zombie.rdata")
